49394349613ccbba3b109234b8a7c5fc7b563111
joe-black-jb <joeblack3601@gmail.com>
2024-06-03 23:45:21 +0900

    マイグレーション用のファイルを作成

---
+++ scripts/migration.go
@@ -0,0 +1,53 @@
+package scripts
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+// TODO migration.go をスクリプトで実行できるようにする
+func main() {
+	enverr := godotenv.Load()
+  if enverr != nil {
+    log.Fatal("Error loading .env file")
+  }
+
+	type User struct {
+		gorm.Model
+		Name  string
+		Age uint
+	}
+
+	dbuser := os.Getenv("MYSQL_USER")
+	dbpass := os.Getenv("MYSQL_ROOT_PASSWORD")
+	dbname := os.Getenv("MYSQL_DATABASE")
+	dbhost := os.Getenv("MYSQL_HOST")
+	// docker コンテナを立ち上げている場合、ホスト名は 127.0.0.1 ではなくサービス名（db）
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:3306)/%s?charset=utf8mb4&parseTime=True&loc=Local", dbuser, dbpass, dbhost, dbname)
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+    log.Fatal("failed to connect database")
+  }
+	// Migrate the schema
+  db.AutoMigrate(&User{})
+
+  // Create
+  db.Create(&User{Name: "Roger Federer", Age: 39})
+  db.Create(&User{Name: "Rafael Nadal", Age: 37})
+
+  // Read
+  var user User
+  db.First(&user, 1) // find product with integer primary key
+  // db.First(&product, "code = ?", "D42") // find product with code D42
+	fmt.Println("user 🎾: ", user)
+ 
+
+	mysql, err := db.DB()
+	mysql.Close()
+	// fmt.Println("Connected⭐️")
+}
