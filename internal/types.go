7166fcdb552881303881c78cde049ac5b511f539
joe-black-jb <joeblack3601@gmail.com>
2024-06-09 10:31:50 +0900

    typeのフィールド修正

--- internal/types.go
+++ internal/types.go
@@ -1,10 +1,15 @@
 package internal
 
-import "gorm.io/gorm"
+import (
+	"gorm.io/gorm"
+)
 
 type Company struct {
 	gorm.Model
 	Name  string
+	Established string
+	Capital string
+	Titles []*Title `gorm:"many2many:company_titles;"`
 }
 
 type Title struct {
@@ -17,7 +22,7 @@ type Title struct {
 	StatementType int
 	Order int `json:"order" gorm:"default:null"`
 	FiscalYear int
-	Value int
+	Value string
 	ParentTitleId int `json:"parent_title_id" gorm:"default:null"`
 }
 
@@ -25,5 +30,5 @@ type CompanyTitle struct {
 	gorm.Model
 	CompanyID  int `gorm:"primaryKey"`
 	TitleID int `gorm:"primaryKey"`
-	Value int
+	Value string
 }
