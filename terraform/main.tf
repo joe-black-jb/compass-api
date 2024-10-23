24f3df6ce65ee48c8addb584bb31057ca02026bf
joe-black-jb <joeblack3601@gmail.com>
2024-10-24 00:02:54 +0900

    terraformファイルを追加

---
+++ terraform/main.tf
@@ -0,0 +1,36 @@
+resource "aws_dynamodb_table" "compass-dynamodb-table" {
+  name           = "compass_companies"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "id"
+
+  attribute {
+    name ="id"
+    type ="S"
+  }
+
+  attribute {
+    name = "name"
+    type = "S"
+  }
+
+  attribute {
+    name = "edinetCode"
+    type = "S"
+  }
+
+  global_secondary_index {
+    name               = "CompanyNameIndex"
+    hash_key           = "name"
+    range_key          = "edinetCode"
+    write_capacity     = 10
+    read_capacity      = 10
+    projection_type    = "INCLUDE"
+    non_key_attributes = ["id"]
+  }
+
+
+  tags = {
+    Name        = "Name"
+    Environment = "compass_companies"
+  }
+}
