e76a6890a5094d55c59a726cf414be9c4276dab7
joe-black-jb <joeblack3601@gmail.com>
2024-11-13 11:33:31 +0900

    MySQLとの接続処理をコメントアウト

--- internal/database/database.go
+++ internal/database/database.go
@@ -1,49 +1,44 @@
 package database
 
 import (
-	"fmt"
-	"log"
-	"os"
-
-	"github.com/joho/godotenv"
-	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 var Db *gorm.DB
 
-func Connect() {
-	fmt.Println("start connection to DB")
+// MySQL を使用しないためコメントアウト
+// func Connect() {
+// 	fmt.Println("start connection to DB")
 
-	env := os.Getenv("ENV")
-	fmt.Println("環境: ", env)
+// 	env := os.Getenv("ENV")
+// 	fmt.Println("環境: ", env)
 
-	if env == "production" {
-		envErr := godotenv.Load()
-		if envErr != nil {
-			log.Fatal("Error loading .env file")
-		}
+// 	if env == "production" {
+// 		envErr := godotenv.Load()
+// 		if envErr != nil {
+// 			log.Fatal("Error loading .env file")
+// 		}
 
-		DbUser := os.Getenv("MYSQL_USER")
-		DbPass := os.Getenv("MYSQL_ROOT_PASSWORD")
-		DbName := os.Getenv("MYSQL_DATABASE")
-		DbHost := os.Getenv("MYSQL_HOST")
+// 		DbUser := os.Getenv("MYSQL_USER")
+// 		DbPass := os.Getenv("MYSQL_ROOT_PASSWORD")
+// 		DbName := os.Getenv("MYSQL_DATABASE")
+// 		DbHost := os.Getenv("MYSQL_HOST")
 
-		type Product struct {
-			gorm.Model
-			Code  string
-			Price uint
-		}
+// 		type Product struct {
+// 			gorm.Model
+// 			Code  string
+// 			Price uint
+// 		}
 
-		// docker コンテナを立ち上げている場合、ホスト名は 127.0.0.1 ではなくサービス名（db）
-		dsn := fmt.Sprintf("%s:%s@tcp(%s:3306)/%s?charset=utf8mb4&parseTime=True&loc=Local", DbUser, DbPass, DbHost, DbName)
-		db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-		if err != nil {
-			log.Fatal("failed to connect database")
-		}
+// 		// docker コンテナを立ち上げている場合、ホスト名は 127.0.0.1 ではなくサービス名（db）
+// 		dsn := fmt.Sprintf("%s:%s@tcp(%s:3306)/%s?charset=utf8mb4&parseTime=True&loc=Local", DbUser, DbPass, DbHost, DbName)
+// 		db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+// 		if err != nil {
+// 			log.Fatal("failed to connect database")
+// 		}
 
-		fmt.Println("Connected⭐️")
-		Db = db
-	}
+// 		fmt.Println("Connected⭐️")
+// 		Db = db
+// 	}
 
-}
+// }
